6f6f3d11119fb018aab1baf000d521b195e69c93
mert özdemir <mertozdemir91@hotmail.com>
2021-11-16 09:54:02 +0300

    create-

--- FlagApp/Controllers/HomeController.cs
+++ FlagApp/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using FlagApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,11 +17,13 @@ namespace FlagApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UygulamaDbContext _db;
+        private readonly IWebHostEnvironment env;
 
-        public HomeController(ILogger<HomeController> logger, UygulamaDbContext db)
+        public HomeController(ILogger<HomeController> logger, UygulamaDbContext db, IWebHostEnvironment env)
         {
             _logger = logger;
             _db = db;
+            this.env = env;
         }
 
         public IActionResult Index()
@@ -30,7 +35,33 @@ namespace FlagApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(FlagCreateViewModel model)
+        {
+            model.Flag.ImageUrl = ResmiKaydet(model.Image);
+            _db.Flags.Add(model.Flag);
+            _db.SaveChanges(); 
+            
 
+            return View();
+        }
+
+        private string ResmiKaydet(IFormFile file)
+        {
+            if (file.Length==0|| file==null)         
+                return null;
+
+            string uzanti = Path.GetExtension(file.FileName);
+            string yeniAd = Guid.NewGuid() + uzanti;
+            string kayitYolu = Path.Combine(env.WebRootPath, "Images", yeniAd);
+
+            using (var fs = System.IO.File.Create(kayitYolu))
+            {
+                file.CopyTo(fs);
+            }
+            return kayitYolu;
+
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ FlagApp/Models/FlagCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlagApp.Models
+{
+    public class FlagCreateViewModel
+    {
+        public Flag Flag { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
